402052b4aa309594879a424fc5584196777d4033
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2020-07-02 11:43:48 +0300

    start

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AngularIS4MySQL.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularIS4MySQL.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
